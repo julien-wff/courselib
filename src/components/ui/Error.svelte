3b90dc4789f2abda0252a37370a8adf06f6b7f43
Julien <cefadrom1@gmail.com>
2020-11-04 19:42:50 +0100

    Added basic Error component

---
+++ src/components/ui/Error.svelte
@@ -0,0 +1,23 @@
+<script>
+    import CentredCard from './CentredCard.svelte';
+
+    export let error;
+
+    $: console.error(error);
+
+    function displayError(err) {
+        if (err instanceof Error) {
+            return `${err.name}: ${err.message}`;
+        } else if (typeof err === 'object') {
+            return JSON.stringify(err, null, 2);
+        } else {
+            return err;
+        }
+    }
+</script>
+
+
+<CentredCard containerClass="2sm:bg-gray-200">
+    <h1 class="mb-4 text-center">Une erreur s'est produite</h1>
+    <p>{displayError(error)}</p>
+</CentredCard>
