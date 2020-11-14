626f15198525b55e5e793d46d7d1df52d3b1d6d2
Julien <cefadrom1@gmail.com>
2020-11-14 15:19:06 +0100

    Added new auth error message (wrong password)

--- src/pages/logged-out/auth-error.js
+++ src/pages/logged-out/auth-error.js
@@ -2,6 +2,8 @@ export function authError(err) {
     switch (err.code) {
         case 'auth/popup-closed-by-user':
             return false;
+        case 'auth/wrong-password':
+            return 'Le mot de passe est invalide, ou une autre méthode de connexion à été utilisée.';
         case 'auth/user-not-found':
             return 'Impossible de trouver un compte correspondant à cet email. Essayez de créer un compte.';
         case 'auth/email-already-in-use':
