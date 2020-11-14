09ac33c26c009836625ef0e6f7da2c3b1d8693bd
Julien <cefadrom1@gmail.com>
2020-11-14 13:43:53 +0100

    Disabled Apple login

--- src/components/ui/SocialLogin.svelte
+++ src/components/ui/SocialLogin.svelte
@@ -2,12 +2,12 @@
     import SocialImg from './SocialImg.svelte';
     import GoogleLogin from '../../images/social/google-login.png';
     import FacebookLogin from '../../images/social/facebook-login.png';
-    import AppleLogin from '../../images/social/apple-login.png';
+    // import AppleLogin from '../../images/social/apple-login.png';
 </script>
 
 
 <div class="w-full flex justify-center my-4">
     <SocialImg src={GoogleLogin} alt="Google" provider="google" on:social-click/>
     <SocialImg src={FacebookLogin} alt="Facebook" provider="facebook" on:social-click/>
-    <SocialImg src={AppleLogin} alt="Apple" provider="Apple" on:social-click/>
+<!--    <SocialImg src={AppleLogin} alt="Apple" provider="Apple" on:social-click/>-->
 </div>
