a0666c3d7e192cd61e83f44d5080e8b12e71f436
Julien <cefadrom1@gmail.com>
2020-11-14 15:15:11 +0100

    Changed the loading animation

--- src/components/ui/CentredCard.svelte
+++ src/components/ui/CentredCard.svelte
@@ -1,4 +1,5 @@
 <script>
+    import Spinner from './Spinner.svelte';
     import { fade } from 'svelte/transition';
     import Container from '../utils/Container.svelte';
     import Center from '../utils/Center.svelte';
@@ -10,16 +11,20 @@
 
 <style>
   .disabled {
-    @apply w-full h-full absolute rounded-md left-0 top-0;
-    background: rgba(.2, .2, .2, .2);
+    background: rgba(.2, .2, .2, .3);
+    backdrop-filter: blur(.04rem);
   }
 </style>
 
 <Container wFull hFull class="{containerClass}">
     <Center>
-        <div class="bg-white 2sm:rounded-md 2sm:shadow-md p-8 w-full 2sm:w-100 2sm:relative {$$props.class || ''}">
+        <div class="bg-white 2sm:rounded-md 2sm:shadow-md 2sm:p-8 p-4 w-full 2sm:w-100 2sm:relative {$$props.class || ''}">
             {#if disabled}
-                <div transition:fade={{duration: 100}} class="disabled"/>
+                <div
+                        transition:fade={{duration: 100}}
+                        class="disabled w-full h-full absolute 2sm:rounded-md left-0 top-0 flex items-center justify-center">
+                    <Spinner variant="light"/>
+                </div>
             {/if}
             <slot/>
         </div>
