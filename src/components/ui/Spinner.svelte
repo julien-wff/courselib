8e6d1817e30a05c3be9e24161c0c6dee9f4adf52
Julien <cefadrom1@gmail.com>
2020-11-14 15:13:51 +0100

    Reworked the spinner

--- src/components/ui/Spinner.svelte
+++ src/components/ui/Spinner.svelte
@@ -1,21 +1,42 @@
-<style>
-    svg {
-      animation: turn 2s ease-in-out infinite;
-      height: 100px;
-      width: 100px;
-    }
+<script>
+    export let variant;
 
-    @keyframes turn {
-      from {
-        transform: rotate(0turn);
-      }
-      to {
-        transform: rotate(3turn);
-      }
+    let stroke;
+
+    $: switch (variant) {
+        case 'light':
+            stroke = '#f7fafc';
+            break;
+        default:
+        case 'dark':
+            stroke = '#718096';
+            break;
     }
+</script>
+
+
+<style>
+  svg {
+    animation: turn 2s ease-in-out infinite;
+    height: 100px;
+    width: 100px;
+  }
+
+  @keyframes turn {
+    from {
+      transform: rotate(0turn);
+    }
+    to {
+      transform: rotate(3turn);
+    }
+  }
 </style>
 
 <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M50 25C50 29.9445 48.5338 34.778 45.7867 38.8893C43.0397 43.0005 39.1352 46.2048 34.5671 48.097C29.9989 49.9892 24.9723 50.4843 20.1227 49.5196C15.2732 48.555 10.8186 46.174 7.32233 42.6777C3.82602 39.1814 1.445 34.7268 0.480368 29.8773C-0.484263 25.0277 0.0108209 20.0011 1.90301 15.4329C3.7952 10.8648 6.99952 6.96029 11.1107 4.21326C15.222 1.46622 20.0555 -5.8963e-08 25 0V25H50Z" fill="#718096"/>
-    <circle cx="25" cy="25" r="22" fill="#EDF2F7"/>
+    <defs>
+        <clipPath id="cut-off-quarter">
+            <path d="M26 1H1V51H51V26H26V1Z"/>
+        </clipPath>
+    </defs>
+    <circle cx="25" cy="25" r="22" fill="none" stroke-width="3" {stroke} clip-path="url(#cut-off-quarter)"/>
 </svg>
