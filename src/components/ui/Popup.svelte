d07fd7a65f91447adc36d88187ce27e6404e62ec
Julien <cefadrom1@gmail.com>
2020-11-14 18:45:58 +0100

    Created popup component

---
+++ src/components/ui/Popup.svelte
@@ -0,0 +1,17 @@
+<script>
+    import CentredCard from './CentredCard.svelte';
+
+    export let disabled;
+</script>
+
+<style>
+  .backdrop {
+    background: rgba(.2, .2, .2, .3);
+  }
+</style>
+
+<div class="absolute top-0 left-0 w-screen h-screen backdrop">
+    <CentredCard {disabled}>
+        <slot/>
+    </CentredCard>
+</div>
