cf18a970e099b1e9ed97ecd3c205f6be26db1b01
Julien <cefadrom1@gmail.com>
2020-12-27 14:08:24 +0100

    Put the backdrop class in the global css

--- src/components/ui/Popup.svelte
+++ src/components/ui/Popup.svelte
@@ -4,11 +4,6 @@
     export let disabled;
 </script>
 
-<style>
-  .backdrop {
-    background: rgba(.2, .2, .2, .3);
-  }
-</style>
 
 <div class="absolute top-0 left-0 w-screen h-screen backdrop">
     <CentredCard {disabled}>
