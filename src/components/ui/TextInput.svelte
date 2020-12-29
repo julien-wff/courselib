937be8364e69144419797f7137cf55fce234bb98
Julien <cefadrom1@gmail.com>
2020-12-29 17:19:40 +0100

    Fixed a bug where the cursor position were reset

--- src/components/ui/TextInput.svelte
+++ src/components/ui/TextInput.svelte
@@ -1,22 +1,17 @@
 <script>
     export let label;
-    export let type = 'text';
     export let value;
     export let noMB = false;
 
-    let props = {};
-    $: props = {
-        ...$$props,
-        type,
-        class: `${noMB ? '' : 'mb-4'}
-        px-4 py-2 w-full rounded border border-gray-400 focus:outline-none focus:border-blue-400 shadow-none
-        ${$$props.class || ''}`
-    };
+    let classes;
+    $: classes = `${noMB ? '' : 'mb-4'}\
+        px-4 py-2 w-full rounded border border-gray-400 focus:outline-none focus:border-blue-400 shadow-none\
+        ${$$props.class || ''}`;
 </script>
 
 <label>
     {#if label}
         <span class="block mb-1">{label}</span>
     {/if}
-    <input {...props} bind:value/>
+    <input {...$$props} class={classes} bind:value/>
 </label>
