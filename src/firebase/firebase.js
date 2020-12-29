ec1c7dc8bb8100a65100db8a0fb3c26965ee5e4a
Julien <cefadrom1@gmail.com>
2020-12-29 14:20:48 +0100

    Replaced firebase analytics with cloudflare analytics

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,11 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
-import 'firebase/analytics';
 import 'firebase/functions';
 import config from './firebase-config.json';
 
 export const initFirebase = () => {
     firebase.initializeApp(config);
-    firebase.analytics();
     if (process.env.NODE_ENV !== 'production')
         firebase.functions().useEmulator(window.location.hostname, 5001);
 
