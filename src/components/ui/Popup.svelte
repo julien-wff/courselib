b6a2bfc10cb0eb7b7160eb8feb9b421f8d8ac2d2
Julien <cefadrom1@gmail.com>
2020-12-29 12:51:17 +0100

    Added a higher z-index

--- src/components/ui/Popup.svelte
+++ src/components/ui/Popup.svelte
@@ -5,7 +5,7 @@
 </script>
 
 
-<div class="absolute top-0 left-0 w-screen h-screen backdrop">
+<div class="absolute top-0 left-0 w-screen h-screen backdrop z-50">
     <CentredCard {disabled}>
         <slot/>
     </CentredCard>
